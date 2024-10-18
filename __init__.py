25153d6e2a48e65e8b5f07d873dacc704b169afc
Abz237 <121437607+Abz237@users.noreply.github.com>
2024-10-18 10:25:09 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -44,7 +44,7 @@ def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
-    data = cursor.fetchall()
+    data = cursor.fetchall() 
     conn.close()
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
